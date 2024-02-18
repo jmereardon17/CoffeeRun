0369d05101705cc664ac7326387fc846db8b0665
Jamie Reardon <jamie.reardon91@gmail.com>
2024-02-18 23:40:14 +0000

    [Bronze Challenge] Truck Id

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,6 @@
   var App = window.App;
   var Truck = App.Truck;
   var DataStore = App.DataStore;
-  var myTruck = new Truck('ncc-1701', new DataStore());
+  var myTruck = new Truck('Galactica', new DataStore());
   window.myTruck = myTruck;
 })(window);
