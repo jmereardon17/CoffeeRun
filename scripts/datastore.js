be0be9341491162cd93369d1f37ca4af350a80f8
Jamie Reardon <jamie.reardon91@gmail.com>
2024-02-19 01:14:40 +0000

    [Silver Challenge] Making data Private

--- scripts/datastore.js
+++ scripts/datastore.js
@@ -1,25 +1,24 @@
 (function (window) {
   'use strict';
   var App = window.App || {};
+  var data = {};
 
-  function DataStore() {
-    this.data = {};
-  }
+  function DataStore() {}
 
   DataStore.prototype.add = function (key, val) {
-    this.data[key] = val;
+    data[key] = val;
   };
 
   DataStore.prototype.get = function (key) {
-    return this.data[key];
+    return data[key];
   };
 
   DataStore.prototype.getAll = function () {
-    return this.data;
+    return data;
   };
 
   DataStore.prototype.remove = function (key) {
-    delete this.data[key];
+    delete data[key];
   };
 
   App.DataStore = DataStore;
