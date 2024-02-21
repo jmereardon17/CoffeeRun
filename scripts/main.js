12ade568ee40861aff1552a9c6aec6557b52f320
Jamie Reardon <jamie.reardon91@gmail.com>
2024-02-21 15:08:38 +0000

    [Silver Challenge] Showing the Value as the Slider Changes

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,19 @@
   var myTruck = new Truck('Galactica', new DataStore());
   window.myTruck = myTruck;
   var formHandler = new FormHandler(FORM_SELECTOR);
+  var slider = $('#strengthLevel');
+  var sliderLabel = slider.prev();
+  var text = sliderLabel.text();
+
+  slider.on('change', function () {
+    sliderLabel.text(text + ' ' + '(' + this.value + ')');
+    sliderLabel.removeClass();
+    if (this.value < 30) {
+      sliderLabel.addClass('text-success');
+    } else if (this.value < 50) {
+      sliderLabel.addClass('text-warning');
+    } else sliderLabel.addClass('text-danger');
+  });
 
   formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
   console.log(formHandler);
